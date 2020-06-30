63732ed718dbfbb373ce7e26e5489ef12fe25121
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-06-30 16:21:50 +0530

    chat-app

---
+++ chat-app/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server);//express does not have access to pass hence raw http server is created 
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection',()=>{
+    console.log('New websocket connection')
+})
+
+server.listen(port,()=>{
+    console.log('Server running on port '+ port);
+})
+
