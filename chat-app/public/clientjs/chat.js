7073f85bcbe555e5594bdbe84dd73a889f648d73
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-07-02 22:40:41 +0530

    rendering user list

--- chat-app/public/clientjs/chat.js
+++ chat-app/public/clientjs/chat.js
@@ -7,6 +7,7 @@ const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sideBarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 const {username,room}=Qs.parse(location.search,{ignoreQueryPrefix:true}) //ignoreQueryPrefix to remove ?
 
@@ -28,6 +29,14 @@ socket.on('locationMessage',(message)=>{
         createdAt:moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+})
+
+socket.on('roomData',({room,users})=>{
+    const html = Mustache.render(sideBarTemplate,{
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit',(e)=>{
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -29,7 +29,10 @@ io.on('connection',(socket)=>{
         socket.join(user.room)
         socket.emit('message',generateMessage(`Welcome ${user.username}`))
         socket.broadcast.to(user.room).emit('message',generateMessage(`${user.username} has joined`)) //send to all users except the sender
-        
+        io.to(user.room).emit('roomData',{
+            room:user.room,
+            users:getUsersInRoom(user.room)
+        })
         callback()
     })
     socket.on('sendMessage',(message,callback)=>{
@@ -48,11 +51,15 @@ io.on('connection',(socket)=>{
         io.to(user.room).emit('locationMessage',generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
-    
+    //updating room data when user leaves
     socket.on('disconnect',()=>{
         const user = removeUser(socket.id)
         if(user){
             io.to(user.room).emit('message',generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('roomData',{
+                room:user.room,
+                users:getUsersInRoom(user.room)
+            })
         }
     })
 })
